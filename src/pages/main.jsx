95185e1023566a96911eaf7c5691ef391bfc5b94
kimeunju <lov.ej.eg.ms@gmail.com>
2023-11-29 10:45:16 +0900

    main

---
+++ src/pages/main.jsx
@@ -0,0 +1,21 @@
+import MainCard from "../components/MainCard";
+import englishData from "../englishData.json";
+
+const Main = () => {
+  return (
+    <div className="bg-lime-100 min-h-screen max-w-screen-md mx-auto px-8 pt-20">
+      <div className="flex justify-center ">
+        <h1 className="rounded-full bg-lime-300 w-[12rem] h-10 flex justify-center items-center text-2xl font-semibold  ">
+          Study English
+        </h1>
+      </div>
+      <ul className="mt-12 px-[7rem] ">
+        {englishData.map((v, i) => (
+          <MainCard key={i} title={v.title} day={v.day} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Main;
